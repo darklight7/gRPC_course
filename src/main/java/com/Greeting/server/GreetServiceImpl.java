bcdf968732503cb61da0dd2bda61ad622210f1a0
Siddhant Nagelia <siddhant.nagelia@olx.com>
2020-07-19 22:16:37 +0530

    Server of Service done

--- src/main/java/com/Greeting/server/GreetServiceImpl.java
+++ src/main/java/com/Greeting/server/GreetServiceImpl.java
@@ -1,4 +1,29 @@
 package com.Greeting.server;
 
-public class GreetServiceImpl {
+import io.grpc.stub.StreamObserver;
+import org.proto.greet.GreetRequest;
+import org.proto.greet.GreetResponse;
+import org.proto.greet.GreetServiceGrpc;
+import org.proto.greet.Greeting;
+
+public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
+
+
+    @Override
+    public void greet(GreetRequest request, StreamObserver<GreetResponse> responseObserver) {
+        Greeting greeting = request.getGreeting();
+        String first_name=greeting.getFirstName();
+
+        String result = "Hellow" + first_name;
+        GreetResponse response = GreetResponse.newBuilder()
+                .setResult(result)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+
+
+
+        //super.greet(request, responseObserver);
+    }
 }
